d4633f04246a4a039061ce54cc652d3035280c98
Beza <bezaworkoffice@gmail.com>
2020-02-13 18:43:37 +0000

    Get blog from medium

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+
+const MEDIUM_URL = "https://medium.com/@codingblackfemales/latest?format=json";
+
+app.get("/blog", (req, res) => {
+  request.get(MEDIUM_URL, (err, apiRes, body) => {
+    if (!err && apiRes.statusCode === 200) {
+      let i = body.indexOf("{");
+      const data = body.substr(i);
+      res.send(data);
+    } else {
+      res.sendStatus(500).json(err);
+    }
+  });
 });
 
 const PORT = process.env.PORT || 4000;
